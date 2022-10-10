af7223f82b6ca6de5fc4af49e839bf260642cf47
Mackenzie Goodluck <108488271+mackenziegoodluck@users.noreply.github.com>
2022-10-10 22:03:54 +0000

    Exercise Patterns.py

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -7,6 +7,29 @@ window.bgcolor('pink')
 Turtle = turtle.Turtle()
 
 turtle.goto(100,100)
+distance = 10
+angle = 90
+is_in_screen = True
+
+colors = ["green", "blue", "purple"]
+
+while is_in_screen:
+  coin = random.randge(0,2)
+if coin:
+  t.right(angle)
+
+else:
+  t.left(angle)
+  t.forward(distance)
 
 
+ turtkeX = t.xcor()
+ turtleY = t.ycor()
 
+ x_range = wn.window_width()/2
+ y_range = wn.window_height()/2
+if abs(turtleX) > x_range or abs(turtleY) > y_range
+ is_in_screen = False
+
+wn.bgcolor ("Yellow")
+wn.exitonclick()
---
+++ ch05/exercises/patterns.py
@@ -0,0 +1,11 @@
+
+def star_pryamid():
+ print("How many rows do you want?")
+ rows = int(input(""))
+
+ for i in range(1,rows + 1):
+  print(i * "*")
+ 
+  
+  
+  
